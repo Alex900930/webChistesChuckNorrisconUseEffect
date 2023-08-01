886b853554362f2c2cc96abd47621699a56a115c
Alexander Herrera <Alex>
2023-08-01 10:13:12 -0400

    Web de Chistes de ChuckNorris con react y useEffect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+
+  const [datos,setDatos] = useState("");
+
+  const handleClick =  () => {
+      const url = "https://api.chucknorris.io/jokes/random";
+      const solicitud = fetch(url);
+        solicitud
+        .then(resp => resp.json())
+        .then(chiste => {
+          setDatos(chiste.value);
+        })
+        .catch(() => console.log('mal'))
+
+  }
+
+  useEffect( ()=>{
+      handleClick();
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+     <h1>Chistes de Chuck Norris:</h1><br />
+     <div className="chiste">{datos}</div> <br />
+     <div className="button-container">
+       <button onClick={handleClick}>Proximo Chiste</button>
+     </div>
+   </>
   );
 }
 
